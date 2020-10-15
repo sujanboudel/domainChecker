2336bcfb31f2945b360d6d38ac00fb086a96253d
sujan boudel <boudelsuzzan@gmail.com>
2020-10-15 11:22:47 +1100

    1.reading domain name from file and checking dates 2.Handling muliple expiry dates

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import whois
 from datetime import datetime
+#opening csv file
+file = open('domainlist.csv', 'r')
 
-w = whois.whois('google.com')
+#reading file
+w = whois.whois(file.readline())
+file.close()
+
+#prints all information about domain
 print(w)
-check_date = w.expiration_date[0]
-print(check_date)
+if type(w.expiration_date) is list:
+    check_date = w.expiration_date[0]
+else:
+    check_date = w.expiration_date
 present = datetime.now()
-print(present)
+#comparing present date and domain expiry date
 if check_date < present:
     print('Domain is expired')
 else:
